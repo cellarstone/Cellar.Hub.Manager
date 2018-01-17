ac78950d96f185506b4bbc3f1b123a5308767aeb
lukaskellerstein <lukas.kellerstein@gmail.com>
2018-01-17 23:14:46 +0100

    change restart 10

--- main.go
+++ main.go
@@ -89,7 +89,7 @@ func init() {
 }
 
 func main() {
-	logger.Information("Cellarstone manager v0.3.17")
+	logger.Information("Cellarstone manager v0.3.18")
 
 	checkCellarDeviceInfo()
 	//killAllNgrokProcesses()
