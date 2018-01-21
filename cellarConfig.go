1a5348f9a644c1014076005d4d47eb0effb0ab59
lukaskellerstein <lukas.kellerstein@gmail.com>
2018-01-21 17:56:02 +0100

    change version and some little changes

--- cellarConfig.go
+++ cellarConfig.go
@@ -11,7 +11,7 @@ import (
 //*****************************************************
 // VERSION
 //*****************************************************
-var cellarVersion = "0.4.2"
+var cellarVersion = "0.4.3"
 var cellarDeviceInfo = ""
 
 var path = "./cellarConfig.txt"
