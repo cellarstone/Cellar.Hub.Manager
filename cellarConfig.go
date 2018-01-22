f5c4e5a035aec64e6bb3e9c93ec525da5297b3f7
lukaskellerstein <lukas.kellerstein@gmail.com>
2018-01-22 17:10:22 +0100

    testing google cloud communication 2

--- cellarConfig.go
+++ cellarConfig.go
@@ -11,7 +11,7 @@ import (
 //*****************************************************
 // VERSION
 //*****************************************************
-var cellarVersion = "0.4.5"
+var cellarVersion = "0.4.6"
 var cellarDeviceInfo = ""
 
 var path = "./cellarConfig.txt"
